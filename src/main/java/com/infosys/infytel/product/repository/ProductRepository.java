8b0871b0687486c76ad754c64763b2e54c0549be
akshay <77652131+Akshay-kanemoni@users.noreply.github.com>
2022-08-04 16:57:39 +0530

    Delete src/main/java/com/infosys/infytel/product directory

--- src/main/java/com/infosys/infytel/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.infytel.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.infytel.product.entity.product;
-
-public interface ProductRepository extends JpaRepository <product, Integer>{
-
-}
--- src/main/java/com/infosys/infytel/product/repository/SubsprodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.infytel.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.infytel.product.entity.subsprod;
-
-public interface SubsprodRepository extends JpaRepository <subsprod , Integer> {
-
-}
